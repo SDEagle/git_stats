97df3c29c9f5d7f6d8094c2a968cb5d2d3468637
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2013-06-28 09:56:48 +0200

    script

---
+++ git_status.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+if Dir.exist? '.git'
+    staged = {}
+    not_staged = {}
+    `git status -s`.split("\n").each do |line| 
+        staged[line[0]] = staged[line[0]].nil? ? 1 : staged[line[0]] + 1 unless line[0] == ' ' || line[1] == '?'
+        not_staged[line[1]] = not_staged[line[1]].nil? ? 1 : not_staged[line[1]] + 1 unless line[1] == ' '
+    end
+    print "(#{not_staged.map{|k,v| "#{k}#{v}"}.join('')}|#{staged.map{|k,v| "#{k}#{v}"}.join('')})" unless staged.empty? && not_staged.empty?
+end
