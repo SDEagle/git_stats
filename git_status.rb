586cead23b4c832b9e26c6076e1a0da856543376
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2013-06-28 10:12:54 +0200

    display clear

--- git_status.rb
+++ git_status.rb
@@ -6,5 +6,9 @@ if Dir.exist? '.git'
         staged[line[0]] = staged[line[0]].nil? ? 1 : staged[line[0]] + 1 unless line[0] == ' ' || line[1] == '?'
         not_staged[line[1]] = not_staged[line[1]].nil? ? 1 : not_staged[line[1]] + 1 unless line[1] == ' '
     end
-    print "(#{not_staged.map{|k,v| "#{k}#{v}"}.join('')}|#{staged.map{|k,v| "#{k}#{v}"}.join('')})" unless staged.empty? && not_staged.empty?
+    if staged.empty? && not_staged.empty?
+    	print '(clear)'
+    else
+    	print "(#{not_staged.map{|k,v| "#{k}#{v}"}.join('')}|#{staged.map{|k,v| "#{k}#{v}"}.join('')})"
+    end
 end
